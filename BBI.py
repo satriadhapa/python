bf39b6ff9ac0de0dce3a8394772f97c15250e17a
apollon <76041403+satriadhapa@users.noreply.github.com>
2021-12-05 18:51:56 +0700

    Update BBI.py

--- BBI.py
+++ BBI.py
@@ -2,18 +2,22 @@ class Ideal:
     def orang(self, nama, tinggi, berat):
         self.nama = str(input("Masukkan nama Anda : "))
         self.nama = nama
-        self.tinggi = int(input("Berapa Tinggi Badan Anda: "))
+            # fungsi dibawah ini merupakan fungsi yang menambahkan value dari user
+            #self.tinggi = int(input("Berapa Tinggi Badan Anda: "))
         self.tinggi = tinggi
-        self.berat = int(input("Berapa berat badan Anda: "))
+            # fungsi dibawah ini merupakan fungsi yang menambahkan value dari user
+            #self.berat = int(input("Berapa berat badan Anda: "))
         self.berat = berat
         ideal = tinggi - 110
         
         if berat == ideal:
-            print(ideal)
-            print("Berat badan Anda sudah ideal. Pertahankan!!!")
+            print("Berat badan Anda sudah ideal. Pertahankan!!!")    
         elif berat > ideal:
             print("Anda perlu diet")
         elif berat < ideal:
             print("Anda kekurangan berat badan")
         else:
             print("Berat badan Anda tidak diketahui.")     
+        print("tinggi badan : ",tinggi)
+        print("Berat badan : ",berat)
+        print("berat badan ideal : ",ideal)    
