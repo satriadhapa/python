21c4149c03735dcb1307edca53892246b27a42e5
apollonsatria <satriadhapa10@gmail.com>
2023-06-24 22:17:50 +0700

    tutorial

---
+++ app.py
@@ -0,0 +1,15 @@
+# print
+print("Moch Satria Dhapa Hamdani")
+print("-" * 10) # expression
+
+# variable
+name = "Moch Satria Dhapa Hamdani" # string data type
+name = "dhapa"
+price = 10000 # integer data type
+rating = 4.9 # float data type
+is_publiced = True # boolean
+print("nama saya ", name)
+
+full_name = 'satria dhapa'
+age = 20
+is_new = True
