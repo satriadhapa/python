9bafbc3052f2376d01b393632d02047b168e9fe7
apollonsatria <satriadhapa10@gmail.com>
2023-06-25 11:42:06 +0700

    update tutorial

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
-# print
-print("Moch Satria Dhapa Hamdani")
-print("-" * 10) # expression
+# # print
+# print("Moch Satria Dhapa Hamdani")
+# print("-" * 10) # expression
 
-# variable
-name = "Moch Satria Dhapa Hamdani" # string data type
-name = "dhapa"
-price = 10000 # integer data type
-rating = 4.9 # float data type
-is_publiced = True # boolean
-print("nama saya ", name)
+# # variable
+# name = "Moch Satria Dhapa Hamdani" # string data type
+# name = "dhapa"
+# price = 10000 # integer data type
+# rating = 4.9 # float data type
+# is_publiced = True # boolean
+# print("nama saya ", name)
 
-full_name = 'satria dhapa'
-age = 20
-is_new = True
+# full_name = 'satria dhapa'
+# age = 20
+# is_new = True
 
-# receiving input
+# # receiving input
 
-name = input("what is your name? ") 
-print("hallo" + name)
+# name = input("what is your name? ") 
+# print("hallo" + name)
 
-# liitle example
-full_name = input("what's your name? ")
-color = input("What's your favourite color? ")
-print(full_name + " likes " + color)
+# # liitle example
+# full_name = input("what's your name? ")
+# color = input("What's your favourite color? ")
+# print(full_name + " likes " + color)
 
+# # type conversion
+# birth = input("birth year: ")
+# age = 2023 - int(birth)
+# print(age)
 
+# # little example
+# berat_lbs = int(input("Berat (lbs): "))
+# berat_kg = berat_lbs * 0.45
+# print(berat_kg, "Kg")
+
+# strings handling
+
+course = "python for 'beginer'"
+print(course)
+# course = '''
+#     Moch Satria Dhapa Hamdani
+#     Teknik Informatika
+# '''
+print(course)
+print(course[-5])
+print(course[0:8])
+
+# litte example
+
+name = "satria"
+print(name[1:-1])
